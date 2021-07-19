7e9444e529b4818a99c60c54fc7448c8b124b164
madhavi3110 <47594154+madhavi3110@users.noreply.github.com>
2021-07-19 15:04:25 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
 print ("Welcome to Docker with Git hub")
+from flask import Flask
+from redis import Redis
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    #count = redis.incr('hits')
+    return '<h1 style="color:red">Welcome to Java Home Docker App</h1>'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
